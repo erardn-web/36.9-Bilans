78b5e0157e5d8074895ae58202c2517ec5987d62
erardn-web <erardn@gmail.com>
2026-03-15 22:08:56 +0100

    Update google_sheets.py

--- utils/google_sheets.py
+++ utils/google_sheets.py
@@ -44,28 +44,25 @@ def get_shv_headers():
     return [
         # Identification
         "bilan_id", "patient_id", "date_bilan", "type_bilan", "praticien", "notes_generales",
-        # HAD – Anxiété (items impairs du questionnaire)
+        # HAD – Anxiété
         "had_a1", "had_a2", "had_a3", "had_a4", "had_a5", "had_a6", "had_a7",
-        # HAD – Dépression (items pairs)
+        # HAD – Dépression
         "had_d1", "had_d2", "had_d3", "had_d4", "had_d5", "had_d6", "had_d7",
         # HAD scores
         "had_score_anxiete", "had_score_depression",
-        # SF-36 – item par item
-        "sf36_q1",
-        "sf36_q2",
-        "sf36_q3a","sf36_q3b","sf36_q3c","sf36_q3d","sf36_q3e",
-        "sf36_q3f","sf36_q3g","sf36_q3h","sf36_q3i","sf36_q3j",
-        "sf36_q4a","sf36_q4b","sf36_q4c","sf36_q4d",
-        "sf36_q5a","sf36_q5b","sf36_q5c",
-        "sf36_q6",
-        "sf36_q7","sf36_q8",
-        "sf36_q9a","sf36_q9b","sf36_q9c","sf36_q9d","sf36_q9e",
-        "sf36_q9f","sf36_q9g","sf36_q9h","sf36_q9i",
-        "sf36_q10",
-        "sf36_q11a","sf36_q11b","sf36_q11c","sf36_q11d",
-        # SF-36 scores dimensionnels (0-100)
-        "sf36_pf","sf36_rp","sf36_bp","sf36_gh",
-        "sf36_vt","sf36_sf","sf36_re","sf36_mh",
+        # SF-12 – items
+        "sf12_q1",
+        "sf12_q2a", "sf12_q2b",
+        "sf12_q3a", "sf12_q3b",
+        "sf12_q4a", "sf12_q4b",
+        "sf12_q5",
+        "sf12_q6a", "sf12_q6b", "sf12_q6c",
+        "sf12_q7",
+        # SF-12 scores dimensionnels (0-100)
+        "sf12_pf", "sf12_rp", "sf12_bp", "sf12_gh",
+        "sf12_vt", "sf12_sf", "sf12_re", "sf12_mh",
+        # SF-12 scores composites
+        "sf12_pcs", "sf12_mcs",
         # BOLT
         "bolt_score", "bolt_interpretation",
         # Test d'hyperventilation volontaire
